db9e1d08483c3ada31f1b4fcd618afdf5e8ab355
wolfcooder <darkcooder@gmail.com>
2020-02-03 16:50:32 +0900

    Initial commit

---
+++ config.js
@@ -0,0 +1,28 @@
+let baseUrl = '';
+module.exports = {
+    port: 3000,
+    localTokens: ['ewrewirjklnhwuer234jkjn434hd'],
+    routes: [
+        {
+            localUrl: '/vkapi',
+            remoteUrl: 'https://vk.com/api/method',
+            localMethod: 'GET',
+            remoteMethod: 'POST',
+            baseParams: {
+                app_id: "554434908",
+                token: "ljkljweklrjewklkmr45"
+            },
+            baseHeaders: []
+        },
+        {
+            localUrl: '/googleapi',
+            remoteUrl: 'https://google.com/api/method',
+            localMethod: 'POST',
+            remoteMethod: 'POST',
+            baseParams: {
+                key: "AIZfrekjlskdukjrkkn34234342m"
+            },
+            baseHeaders: []
+        }
+    ]
+};
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')();
+const request = require('request');
+const config = require('./config');
+
+
+
+express.get('/', function (req, res) {
+    res.send('Welcome to simple ApiProxy!')
+});
+
+config.routes.forEach((route) => {
+    let callback = function (req, res) {
+        if(config.localTokens.indexOf(req.token) === -1) {
+            res.status(401).send("Unknown token");
+            return;
+        }
+        let request = Object.assign(req, route.baseParams);
+        request({
+            method: route.remoteMethod,
+            uri: route.remoteUrl,
+            form: request,
+            headers: route.baseHeaders
+        }, function(error, response, body) {
+            if(error) res.error(error);
+            res.status(response && response.status).send(body);
+        })
+    };
+    switch(route.localMethod) {
+        case "POST":
+            express.post(route.localUrl,callback);
+            break;
+        case "PUT":
+            express.put(route.localUrl, callback);
+            break;
+        case "DELETE":
+            express.delete(route.localUrl, callback);
+            break;
+        default:
+            express.get(route.localUrl, callback);
+    }
+});
+
+express.listen(config.port);
+console.log ("Proxy input on port " + config.port);
